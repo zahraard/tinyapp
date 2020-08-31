001aa54f7bb5fc24a06935c71b3aa4d75970d95f
Zahra Ardalani <zahra.ardalani@gmail.com>
2020-08-31 21:50:06 +0000

    template added.

--- express_server.js
+++ express_server.js
@@ -7,28 +7,22 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.set("view engine", "ejs");
+
 app.get('/', (req, res)=>{
   res.send("Hello!")
 });
 
-app.get('/urls.json', (req, res)=>{
-  res.json(urlDatabase);
+app.get('/urls', (req, res)=>{
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars)
 })
 
-app.get('/hello', (req, res)=>{
-  res.send("<html><body>Hello <b>World</b></body></html>\n")
-})
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
 
- 
+
+
+
 app.listen(PORT, ()=>{
   console.log(`Example app listening on port ${PORT}!`);
 })
