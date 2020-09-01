281601d4d16d6175e1b0cc17916058073d8910ab
Zahra Ardalani <zahra.ardalani@gmail.com>
2020-09-01 17:41:55 +0000

    CRUD added

--- express_server.js
+++ express_server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+
+function generateRandomString() {
+  let rand = Math.random().toString(36).substring(7, 1);
+  return rand;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -8,6 +14,7 @@ const urlDatabase = {
 };
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res)=>{
   res.send("Hello!")
@@ -18,6 +25,10 @@ app.get('/urls', (req, res)=>{
   res.render("urls_index", templateVars)
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
@@ -25,7 +36,37 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
+});
 
+app.post("/urls", (req, res) => {
+  const generatedUrl = generateRandomString();
+  urlDatabase[generatedUrl] = req.body.longURL;  
+  console.log(urlDatabase);  // Log the POST request body to the console
+  res.redirect(`/urls/${generatedUrl}`);
+  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.post('/urls/:shortURL', (req, res)=>{
+  const shortURL = req.params.shortURL;
+  for(let url in urlDatabase){
+    if(url === shortURL)[
+      urlDatabase[shortURL] = req.body.newURL
+    ]
+  }
+  console.log('url: ',urlDatabase)
+  res.redirect('/urls');
+})
+
+app.post(('/urls/:shortURL/delete'), (req, res)=>{
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  console.log('url: ',urlDatabase)
+  res.redirect('/urls');
+})
 app.listen(PORT, ()=>{
   console.log(`Example app listening on port ${PORT}!`);
 })
